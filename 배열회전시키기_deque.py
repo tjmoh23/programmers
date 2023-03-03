fbdcad912b428943845ca3beb45258989ebee6d6
이호연 <51068987+tjmoh23@users.noreply.github.com>
2023-03-03 23:54:07 +0900

    230303 Algorithm Commit

---
+++ 배열회전시키기_deque.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+def solution(numbers, direction):
+    numbers = deque(numbers)
+    if direction == "right":
+        numbers.rotate(1)
+    elif direction == "left":
+        numbers.rotate(-1)
+        
+    return list(numbers)
+  
+  # deque()
+  # 함수 안에 양수를 넣으면 오른쪽 회전, 음수를 넣으면 왼쪽 회전
