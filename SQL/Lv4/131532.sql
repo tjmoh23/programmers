fa0f9922fe9062c01911c5f04da3af4318ee05c6
이호연 <51068987+tjmoh23@users.noreply.github.com>
2023-06-26 23:29:39 +0900

    년, 월, 성별별 상품 구매 회원 수 구하기

--- SQL/Lv4/131532.sql
+++ SQL/Lv4/131532.sql
@@ -1,6 +1,7 @@
 -- 년, 월, 성별 별로 상품을 구매한 회원수를 집계하는 SQL문을 작성
 -- 성별 정보가 없는 경우 결과에서 제외
 
+
 SELECT DATE_FORMAT(O.SALES_DATE, '%Y') AS YEAR,
         DATE_FORMAT(O.SALES_DATE, '%m') AS MONTH,
         U.GENDER, COUNT(DISTINCT U.USER_ID) AS USERS
@@ -9,6 +10,7 @@ JOIN ONLINE_SALE O ON U.USER_ID = O.USER_ID
 WHERE U.GENDER IS NOT NULL
 GROUP BY YEAR, MONTH, GENDER
 ORDER BY YEAR, MONTH, GENDER
+
 
 -- 처음에는 DISTINCT를 사용하지 않고 USER_ID를 COUNT하였는데 자꾸 오류가 발생했다.
 -- DISTINCT를 넣어주었더니 오류 해결
