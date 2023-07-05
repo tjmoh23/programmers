acbdd642c1c159ee8e47d44961300d08bd516bb1
이호연 <51068987+tjmoh23@users.noreply.github.com>
2023-07-05 23:47:30 +0900

    즐겨찾기가 가장 많은 식당 정보 출력하기 - 왜 오답인지 잘 알아두기 (GROUP BY와 MAX)

--- SQL/Lv3/131123.sql
+++ SQL/Lv3/131123.sql
@@ -17,6 +17,9 @@ ORDER BY FOOD_TYPE DESC
 -- 서브쿼리 만들지 않고 한 번에 할 수 있지 않을까 했는데 계속 안 됐다
 -- 이렇게 실행할 경우, 즐겨찾기가 가장 많은 식당의 정보가 아닌 테이블에서 가장 상단의 식당 정보를 가져오는 거다
 -- 정답 풀이와 왜 다를까? ? ?
+-- -> 그룹 바이한 컬럼 외에 다른 컬럼은 올 수 없다! 애초에 에러가 나는 문법인데 MySQL에서만 예외적으로 실행이 되었던 거다
+-- MAX(FAVORITES)는 옳게 나왔지만 나머지 컬럼들은 모두 FOOD_TYPE마다 가장 위에 행이 출력됨
+-- 맞지 않은 데이터다
 SELECT FOOD_TYPE, REST_ID, REST_NAME, MAX(FAVORITES) AS FAVORITES
 FROM REST_INFO
 GROUP BY FOOD_TYPE
