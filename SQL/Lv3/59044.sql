9525e31d7a6a0d750cca0a5dcc849befc4758972
이호연 <51068987+tjmoh23@users.noreply.github.com>
2023-07-19 21:27:39 +0900

    오랜 기간 보호한 동물(1)

--- SQL/Lv3/59044.sql
+++ SQL/Lv3/59044.sql
@@ -5,7 +5,7 @@ FROM
     (
     SELECT 
     NAME, DATETIME, 
-    DENSE_RANK() OVER(ORDER BY DATETIME) AS RANK
+    ROW_NUMBER() OVER(ORDER BY DATETIME) AS RANK -- ※ 주의!
     FROM ANIMAL_INS
     WHERE ANIMAL_ID NOT IN (SELECT ANIMAL_ID
                             FROM ANIMAL_OUTS)
@@ -13,4 +13,6 @@ FROM
 WHERE RANK IN (1, 2, 3)
 
 -- WHERE NOT IN 사용하는 방법!
+-- ★DENSE_RANK가 아니라 ROW_NUMBER를 사용해야 한다!★
+-- 공동 순위가 있을 경우, DENSE_RANK를 사용하면 3마리 이상의 값이 나올 수 있다.
 -- 문제 출처: https://school.programmers.co.kr/learn/courses/30/lessons/59044
