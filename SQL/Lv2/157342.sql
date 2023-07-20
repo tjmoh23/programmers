6c1e3fe3487c55c69066a6ca1ac5a36535087f69
이호연 <51068987+tjmoh23@users.noreply.github.com>
2023-07-20 17:55:14 +0900

    자동차 평균 대여 기간 구하기

--- SQL/Lv2/157342.sql
+++ SQL/Lv2/157342.sql
@@ -1,5 +1,7 @@
 -- 평균 대여 기간이 7일 이상인 자동차들의 자동차 ID와 평균 대여 기간 출력
+-- 소수점 두 번째 자리에서 반올림할 경우 ROUND(변수이름, 1)로 표현한다! 2가 아니라 1이다!
 
+-- 내 풀이
 SELECT *
 FROM(
     SELECT 
@@ -19,6 +21,17 @@ FROM(
 WHERE AVERAGE_DURATION >= 7
 ORDER BY AVERAGE_DURATION DESC, CAR_ID DESC
 
+    
 
--- 소수점 두 번째 자리에서 반올림할 경우 ROUND(변수이름, 1)로 표현한다! 2가 아니라 1이다!
+-- 더 간단한 풀이
+-- GROUP BY 하면 집계함수 쓸 수 있으니 여기서도 바로 SELECT문에서 AVG 쓸 수 있는 거다..
+SELECT
+    CAR_ID,
+    ROUND(AVG(END_DATE - START_DATE + 1), 1) AS AVERAGE_DURATION
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+GROUP BY CAR_ID
+HAVING ROUND(AVG(END_DATE-START_DATE+1), 1) >= 7
+ORDER BY AVERAGE_DURATION DESC, CAR_ID DESC
+
+
 -- 문제 출처: https://school.programmers.co.kr/learn/courses/30/lessons/157342
