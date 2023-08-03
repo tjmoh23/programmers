62e8794692cf8e9ffe87941d07ac4fb812e5ff56
이호연 <51068987+tjmoh23@users.noreply.github.com>
2023-08-03 09:48:56 +0900

    자동차 종류별 특정 옵션이 포함된 자동차 수 구하기 - IN 의미 헷갈리는 거 확인 완료

--- SQL/Lv2/151137.sql
+++ SQL/Lv2/151137.sql
@@ -7,4 +7,9 @@ WHERE OPTIONS LIKE '%시트%' -- WHERE OPTIONS IN ('통풍시트', '열선시트
 GROUP BY CAR_TYPE
 ORDER BY CAR_TYPE
 
+
+-- IN은 포함이 아니라 (= OR) 의 의미이다.
+-- 컬럼값이 통풍시트인지, 열선시트인지, 가죽시트인지 하나 하를 확인하는 거다.
+-- ★IN은 포함된 게 있는지 보는 게 아니다!!!!!!!★
+
 -- 문제 출처: https://school.programmers.co.kr/learn/courses/30/lessons/151137
