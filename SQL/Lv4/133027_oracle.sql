12990bcd733a8e331afa2c19f6516bcab25c3df6
이호연 <51068987+tjmoh23@users.noreply.github.com>
2023-08-10 09:12:59 +0900

    주문량이 많은 아이스크림들 조회하기

--- SQL/Lv4/133027_oracle.sql
+++ SQL/Lv4/133027_oracle.sql
@@ -17,6 +17,28 @@ FROM(
     GROUP BY FLAVOR
     ORDER BY TOTAL_ORDER DESC
     )
-FETCH FIRST 3 ROWS WITH TIES
+FETCH FIRST 3 ROWS WITH TIES -- 버전에 따라 사용 못 할 수 도 있다. 원래 순위 관련해서 몇 개만 추출할 경우 WINDOW 함수로 무조건 사용했어야 했음!
+
+
+
+-- 상빈님 문제 풀이
+
+SELECT FLAVOR
+FROM (
+SELECT 
+    F.FLAVOR, SUM(J.TOTAL_ORDER + F.TOTAL_ORDER) AS TOTAL_ORDER,
+    ROW_NUMBER() OVER(ORDER BY SUM(J.TOTAL_ORDER + F.TOTAL_ORDER) DESC) AS RN
+FROM FIRST_HALF F, 
+(
+SELECT FLAVOR, SUM(TOTAL_ORDER) AS TOTAL_ORDER
+FROM JULY
+GROUP BY FLAVOR
+) J
+WHERE F.FLAVOR = J.FLAVOR
+GROUP BY F.FLAVOR
+    )
+WHERE RN <= 3
+
+
 
 -- 문제 출처: https://school.programmers.co.kr/learn/courses/30/lessons/133027?language=oracle
