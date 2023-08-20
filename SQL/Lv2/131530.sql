2d43449ec31b9982ea2d23a3fcffa27acebceb77
이호연 <51068987+tjmoh23@users.noreply.github.com>
2023-08-21 00:04:48 +0900

    가격대별 상품 개수 구하기 -- 오답 확인하기

--- SQL/Lv2/131530.sql
+++ SQL/Lv2/131530.sql
@@ -1,1 +1,16 @@
+
+SELECT 
+    PRICE_GROUP,
+    COUNT(PRODUCT_CODE) AS PRODUCTS
+FROM
+    (
+        SELECT
+            ROUND((PRICE/10000), 0)*10000 AS PRICE_GROUP,
+            PRODUCT_CODE
+        FROM PRODUCT
+    )
+GROUP BY PRICE_GROUP
+ORDER BY PRICE_GROUP 
+
+-- 왜 오답일까?
 -- 문제 출처: https://school.programmers.co.kr/learn/courses/30/lessons/131530
