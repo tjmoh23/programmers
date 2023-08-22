d89a360f7e3dc3eb72c9e3404835c8992056a720
이호연 <51068987+tjmoh23@users.noreply.github.com>
2023-08-22 09:23:05 +0900

    가격대별 상품 개수 구하기

--- SQL/Lv2/131530.sql
+++ SQL/Lv2/131530.sql
@@ -1,4 +1,6 @@
 
+-- 내 풀이
+
 SELECT 
     PRICE_GROUP,
     COUNT(PRODUCT_CODE) AS PRODUCTS
@@ -13,4 +15,6 @@ GROUP BY PRICE_GROUP
 ORDER BY PRICE_GROUP 
 
 -- 왜 오답일까?
+-- ROUND는 반올림을 하기 때문에 1.5 이상부터는 2로 계산하므로 오답이다!
+-- TRUNC 함수를 사용해야한다
 -- 문제 출처: https://school.programmers.co.kr/learn/courses/30/lessons/131530
