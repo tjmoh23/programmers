3f032e931748944c687e830b314b1f0e49bd48f2
이호연 <51068987+tjmoh23@users.noreply.github.com>
2023-08-29 21:10:43 +0900

    대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기 -- 여러 방법으로 풀어보기(마지막 방법 다시 해보기)

--- SQL/Lv3/151139.sql
+++ SQL/Lv3/151139.sql
@@ -44,4 +44,48 @@ ORDER BY MONTH, CAR_ID DESC;
 -- 즉, 위에서 WHERE절로 시간을 한 번 조건을 걸어줬더라도, WHERE IN 절에서의 SELECT절은 별개의 조건이기 때문에 한 번 더 날짜 조건을 걸어 줘야하는 거임!!
 
 
+
+
+2023. 08. 29. 다른 풀이 1
+-- 정답 포맷이 달라서 자꾸 오답처리되었었음
+-- 성능이 좋지 못한 풀이
+SELECT 
+    TO_NUMBER(TO_CHAR(START_DATE, 'MM')) AS MONTH,  -- TO_NUMBER로 한번 더 감싸서 정답 포맷으로 맞춰준다! (01 -> 1)
+    CAR_ID,
+    COUNT(HISTORY_ID) AS RECORDS
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+WHERE CAR_ID IN (
+                    SELECT CAR_ID
+                    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+                    WHERE TO_CHAR(START_DATE, 'YYYY-MM') BETWEEN '2022-08' AND '2022-10' 
+                    GROUP BY CAR_ID
+                    HAVING COUNT(CAR_ID) >= 5
+                )
+AND TO_CHAR(START_DATE, 'YYYY-MM') BETWEEN '2022-08' AND '2022-10'                
+GROUP BY TO_CHAR(START_DATE, 'MM'), CAR_ID
+HAVING COUNT(HISTORY_ID) != 0
+ORDER BY MONTH, CAR_ID DESC
+
+
+2023. 08. 29. 다른 풀이 2
+-- 성능이 위보다 더 좋은 풀이
+-- 인라인 뷰 + 윈도우 함수
+SELECT
+    TO_NUMBER(MONTH) AS MONTH, CAR_ID, RECORDS
+FROM (
+    SELECT DISTINCT
+        CAR_ID, 
+        TO_CHAR(START_DATE, 'MM') AS MONTH,
+        COUNT(HISTORY_ID) OVER(PARTITION BY CAR_ID) AS CNT, -- 8월부터 10월까지 자동차별 총 대여횟수
+        COUNT(HISTORY_ID) OVER(PARTITION BY TO_CHAR(START_DATE, 'MM'), CAR_ID) AS RECORDS -- 8월부터 10월까지 월별 자동차별 대여횟수
+    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY 
+    WHERE START_DATE
+    BETWEEN TO_DATE('2022-08-01', 'YYYY-MM-DD')
+        AND TO_DATE('2022-10-31', 'YYYY-MM-DD')
+    )
+WHERE CNT >= 5
+ORDER BY MONTH, CAR_ID DESC;
+
+
+
 -- 문제 출처: https://school.programmers.co.kr/learn/courses/30/lessons/151139
