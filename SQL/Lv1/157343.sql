e3d5ca6caba5cb593dd5f82f9a8be7fba10d6b7b
이호연 <51068987+tjmoh23@users.noreply.github.com>
2023-08-30 15:20:57 +0900

    특정 옵션이 포함된 자동차 리스트 구하기 -- 정규표현식

--- SQL/Lv1/157343.sql
+++ SQL/Lv1/157343.sql
@@ -1,14 +1,20 @@
-
-    SELECT 
-        REGEXP_SUBSTR(OPTIONS, '[^,]+', 1, 1) AS "구분1",
-        REGEXP_SUBSTR(OPTIONS, '[^,]+', 1, 2) AS "구분2",
-        REGEXP_SUBSTR(OPTIONS, '[^,]+', 1, 3) AS "구분3",
-        REGEXP_SUBSTR(OPTIONS, '[^,]+', 1, 4) AS "구분4",
-        REGEXP_SUBSTR(OPTIONS, '[^,]+', 1, 5) AS "구분5",
-        REGEXP_SUBSTR(OPTIONS, '[^,]+', 1, 6) AS "구분6"
+-- 옵션 중에서 '열선시트'가 하나라도 포함된 자동차 ID와 옵션1~6을 조회하는 쿼리
+SELECT
+    *    
+FROM(
+    SELECT
+        CAR_ID,
+        REGEXP_SUBSTR(OPTIONS, '[^,]+', 1, 1) AS OPTION1,
+        REGEXP_SUBSTR(OPTIONS, '[^,]+', 1, 2) AS OPTION2,
+        REGEXP_SUBSTR(OPTIONS, '[^,]+', 1, 3) AS OPTION3,
+        REGEXP_SUBSTR(OPTIONS, '[^,]+', 1, 4) AS OPTION4,
+        REGEXP_SUBSTR(OPTIONS, '[^,]+', 1, 5) AS OPTION5,
+        REGEXP_SUBSTR(OPTIONS, '[^,]+', 1, 6) AS OPTION6
     FROM CAR_RENTAL_COMPANY_CAR
-    ORDER BY "구분6", "구분5", "구분4", "구분3", "구분2", "구분1"
+    ORDER BY OPTION6, OPTION5, OPTION4, OPTION3, OPTION2, OPTION1
+    )
+WHERE '열선시트' = ANY(OPTION1, OPTION2, OPTION3, OPTION4, OPTION5, OPTION6)
 
 
--- LIKE를 사용하여 푸는 간단한 문제지만, 정규표현식을 사용하여 문제를 풀어보도록 하자. 아직 안 풀었음!  
+-- LIKE를 사용하여 푸는 간단한 문제이지만 이런 식으로 정규 표현식을 사용해볼 수 있다.
 -- 문제 출처: https://school.programmers.co.kr/learn/courses/30/lessons/157343?language=oracle
